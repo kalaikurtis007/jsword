b7b48dafa313f451a4dc62d7c5c3317be425831d
kalaiselvan.a <kalaikurtis007@gmail.com>
2017-02-09 18:40:10 +0530

    bind event

--- jsword.js
+++ jsword.js
@@ -27,7 +27,7 @@
                 options = $.extend(defaultOptions, options);
 
                 impl.appendGrid(options);
-
+				impl.bindEvents();
                 data.hide();
                 data.css('visibility', 'visible');
                 data.fadeIn('slow');
@@ -59,8 +59,7 @@
 
                     for (j = 0; j < gridSize.width; j++) {
                         var tile = newTile(i, j).appendTo(row);
-                        if (typeof (options.gridMask[i]) !== 'undefined' && options.gridMask[i].charAt(j) === ' ') {
-							console.log("active");
+                        if (typeof (options.gridMask[i]) !== 'undefined' && options.gridMask[i].charAt(j) === ' ') {							
                             tile.addClass('cwd-tile-active').css('background-color', 'White');
                         }
                         else {
@@ -68,15 +67,74 @@
                         }
                     }
                 }
+				var index=0,acrossClueIndex=0, downClueIndex=0;
+				grid.find('td.cwd-tile-active').each(function(){
+					var i=parseInt($(this).attr('row'));
+					var j=parseInt($(this).attr('col'));
+					 var acrossNo = (j === 0 || options.gridMask[i].charAt(j - 1) !== ' ') &&
+                                (j < gridSize.width - 1 && options.gridMask[i].charAt(j + 1) === ' ')
+
+                    // Inactive tile above (or edge) AND active tile below => start of down clue.
+                    var downNo = (i === 0 || options.gridMask[i - 1].charAt(j) !== ' ') &&
+                                (i < gridSize.height - 1 && options.gridMask[i + 1].charAt(j) === ' ')
+								
+					var acrossNo=(j===0 || options.gridMask[i].charAt(j-1)!==' ' && (j<gridSize.width-1 && options.gridMask[i].charAt(j+1)===' '))
+					var downNo=(i===0 || options.gridMask[i-1].charAt(j)!==' ') && ( i<gridSize.height-1 && options.gridMask[i+1].charAt(j)===' ')
+					var row=i,col=j;
+					if(acrossNo){
+						while(col<gridSize.width){
+							var thisTile=grid.find(impl.stringFormat('td[row={0}][col={0}]',row,col));
+							if(thisTile.hasClass('cwd-tile-inactive')){
+								break;
+							}else{
+								thisTile.attr('acrossClueId',index);
+							}
+							col++;
+						}						
+					}
+					row=i;
+					col=j;
+					if(downNo){
+						while(row < gridSize.height){
+							var thisTile=grid.find(impl.stringFormat('td[row={0}][col={1}]',row,col));
+							if(thisTile.hasClass('cwd-tile-inactive')){
+								break;
+							}else{
+								thisTile.attr('downClueId',index);
+							}
+							row++;
+						}
+					}
+					if(acrossNo||downNo){
+						$(this).prepend($('<span/>').text(clueNo++).css({'position':'absolute','font-size':options.tileSize/3}));
+					}
+					
+				})
 				//console.log(data);
-                var container=$('<div/>').attr('id','cwd-container').appendTo(data);
-				console.log(container);
+                var container=$('<div/>').attr('id','cwd-container').appendTo(data);				
                 $('<div id="cwd-divGrid"/>')
                   .append(grid)
                   .appendTo(container)
                 .css({display:"inline-block",width:grid.width()});
 
             },
+			bindEvents:function(){
+				var s=this;
+				$('#cwd-grid td.cwd-tile-active').click(function(){					
+					var preDirection="";
+					if($(this).hasClass('cwd-tile-highlight')){						 
+						preDirection=$(this).prev('td.cwd-tile-active').hasClass('cwd-tile-highlight')|| $(this).prev('td.cwd-tile-active').hasClass('cwd-tile-highlight')?'a':'d';
+					}
+				});
+				$(document).keydown(function(event){
+					var highlightedTiles=$('#cwd-grid td.cwd-tile-highlight');
+					if(highlightedTiles.length>0){
+						
+					}else{
+						alert();
+					}
+				});
+			},
             stringFormat: function () {
                 var resultString = arguments[0];
                 var args = arguments;
